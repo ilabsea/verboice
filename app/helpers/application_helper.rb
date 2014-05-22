8704f416b4f83ec710edb436d54f0f2af9e6add5
Kakada Chheang <kakada.chheang@gmail.com>
2014-05-22 16:35:11 +0700

    add setting to enable/disable speech_recognition step(fixes #48)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -97,6 +97,10 @@ module ApplicationHelper
     Pigeon.config.nuntium_configured?
   end
 
+  def steps_configured?
+    STEP_CONFIG["speech_recognition"] == true
+  end
+
   def datetime_format(datetime, time_zone)
     datetime = datetime.try(:in_time_zone, time_zone || 'UTC')
     datetime.present? ? datetime.strftime(Time::DEFAULT_FORMAT) : ''
