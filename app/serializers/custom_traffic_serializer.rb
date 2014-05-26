58e7f99181bf14cc361de40df466bc665136f9ef
Kakada Chheang <kakada.chheang@gmail.com>
2014-05-26 14:07:38 +0700

    modify traffic serializer to expose account and channel serializer

--- app/serializers/custom_traffic_serializer.rb
+++ app/serializers/custom_traffic_serializer.rb
@@ -16,11 +16,13 @@
 # along with Verboice.  If not, see <http://www.gnu.org/licenses/>.
 
 class CustomTrafficSerializer < ActiveModel::Serializer
-  attributes :address, :total_duration, :direction, :channel_id, :account_id
+  attributes :address, :total_duration, :direction
+
+  has_one :account, serializer: CustomAccountSerializer
+  has_one :channel, serializer: CustomChannelSerializer
 
   def address
-    return address = object.address if object.prefix_called_number.nil? || object.address.nil?
-    return address = object.address[object.prefix_called_number.size..-1] if object.address.start_with? object.prefix_called_number
+    object.address_without_prefix
   end
 
 end
