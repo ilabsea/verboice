137261cdcf60a47efc8304880958dd61357ce1ed
Kakada Chheang <kakada.chheang@gmail.com>
2014-08-27 14:59:40 +0700

    add total and used amount to channel quota

--- app/models/channel_quota.rb
+++ app/models/channel_quota.rb
@@ -2,7 +2,7 @@ class ChannelQuota < ActiveRecord::Base
   set_table_name 'channel_quotas'
   
   belongs_to :channel
-  attr_accessible :blocked, :enabled
+  attr_accessible :blocked, :enabled, :total, :used
 
   validates_uniqueness_of :channel_id
 
