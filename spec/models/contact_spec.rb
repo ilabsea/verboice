8e143366de341c46dcdafa3063f91218b1638c08
Kakada Chheang <kakada.chheang@gmail.com>
2014-11-04 16:32:54 +0700

    remove duplicate address and keep the last updated

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,3 +1,20 @@
+# Copyright (C) 2010-2012, InSTEDD
+#
+# This file is part of Verboice.
+#
+# Verboice is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Verboice is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Verboice.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'spec_helper'
 
 describe Contact do
@@ -77,6 +94,195 @@ describe Contact do
 
     it "should return nil when it doesn't have 9999" do
       Contact.get(9999, @project).should be_nil
+    end
+  end
+
+  describe "#last_updated_value_than?" do
+    context "return true when the right variable value is nil" do
+      before(:each) do
+        @contact1 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "1000"), ContactAddress.create(project: project, address: "2000")]
+        @contact2 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "3000"), ContactAddress.create(project: project, address: "8551000")]
+
+        PersistedVariable.make contact: @contact1, project: project, project_variable: project_var1, updated_at: Time.now
+      end
+
+      it { expect(@contact1.last_updated_value_than?(@contact2, project_var1)).to eq(true) }
+    end
+
+    context "return false when the left variable value is nil" do
+      before(:each) do
+        @contact1 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "1000"), ContactAddress.create(project: project, address: "2000")]
+        @contact2 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "3000"), ContactAddress.create(project: project, address: "8551000")]
+
+        PersistedVariable.make contact: @contact2, project: project, project_variable: project_var1, updated_at: Time.now
+      end
+
+      it { expect(@contact1.last_updated_value_than?(@contact2, project_var1)).to eq(false) }
+    end
+
+    context "return true when the left variable value is last modified" do
+      before(:each) do
+        @contact1 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "1000"), ContactAddress.create(project: project, address: "2000")]
+        @contact2 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "3000"), ContactAddress.create(project: project, address: "8551000")]
+
+        PersistedVariable.make contact: @contact1, project: project, project_variable: project_var1, updated_at: Time.now + 1.days
+        PersistedVariable.make contact: @contact2, project: project, project_variable: project_var1, updated_at: Time.now
+      end
+
+      it { expect(@contact1.last_updated_value_than?(@contact2, project_var1)).to eq(true) }
+    end
+
+    context "return false when the right variable value is last modified" do
+      before(:each) do
+        @contact1 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "1000"), ContactAddress.create(project: project, address: "2000")]
+        @contact2 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "3000"), ContactAddress.create(project: project, address: "8551000")]
+
+        PersistedVariable.make contact: @contact1, project: project, project_variable: project_var1, updated_at: Time.now
+        PersistedVariable.make contact: @contact2, project: project, project_variable: project_var1, updated_at: Time.now + 1.days
+      end
+
+      it { expect(@contact1.last_updated_value_than?(@contact2, project_var1)).to eq(false) }
+    end
+    
+  end
+
+  describe ".remove_duplicate_address_and_keep_last_update" do
+    context "keep the first one when it's no varaible value" do
+      before(:each) do
+        @contact1 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "01000")]
+        @contact11 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "1000")]
+      end
+
+      it { expect(Contact.count).to eq(2)}
+
+      it "should remove duplicated contacts when it has only one address" do
+        Contact.remove_duplicate_address_and_keep_last_update(project, project_var1)
+
+        expect(Contact.count).to eq(1)
+      end
+
+      it "should keep the first one" do
+        Contact.remove_duplicate_address_and_keep_last_update(project, project_var1)
+
+        expect(Contact.last.id).to eq(@contact1.id)
+      end
+    end
+
+    context "remove only address which is duplicated when it has many addresses" do
+      before(:each) do
+        @contact1 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "01000")]
+        @contact11 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "1000")]
+        @contact3 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "8551000"), ContactAddress.create(project: project, address: "3000")]
+      end
+
+      it { expect(Contact.count).to eq(3) }
+
+      it "should remove contact11" do
+        Contact.remove_duplicate_address_and_keep_last_update(project, project_var1)
+
+        expect(Contact.count).to eq(2)
+      end
+
+      it "should keep the first one" do
+        Contact.remove_duplicate_address_and_keep_last_update(project, project_var1)
+
+        expect(Contact.first.id).to eq(@contact1.id)
+      end
+
+      it "should remove contact address of the contact3" do
+        Contact.remove_duplicate_address_and_keep_last_update(project, project_var1)
+
+        expect(Contact.last.id).to eq(@contact3.id)
+      end
+    end
+
+    context "keep the existing variable value" do
+      context "duplicate primary address" do
+        before(:each) do
+          @contact1 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "01000")]
+          @contact11 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "1000")]
+
+          PersistedVariable.make contact: @contact11, project: project, project_variable: project_var1, updated_at: Time.now
+        end
+
+        it "should the last when it has variable value" do
+          Contact.remove_duplicate_address_and_keep_last_update(project, project_var1)
+
+          expect(Contact.last.id).to eq(@contact11.id)
+        end
+      end
+
+      context "duplicate secondary address" do
+        it "should remove contact address of the first contact" do
+          @contact1 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "3000"), ContactAddress.create(project: project, address: "8551000")]
+          @contact11 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "1000")]
+
+          PersistedVariable.make contact: @contact11, project: project, project_variable: project_var1, updated_at: Time.now
+
+          Contact.remove_duplicate_address_and_keep_last_update(project, project_var1)
+
+          expect(Contact.first.id).to eq(@contact1.id)
+          expect(@contact1.reload.addresses.count).to eq(1)
+          expect(Contact.last.id).to eq(@contact11.id)
+        end
+
+        it "should remove the first contact when it has variable value" do
+          @contact1 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "01000")]
+          @contact11 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "3000"), ContactAddress.create(project: project, address: "1000")]
+
+          PersistedVariable.make contact: @contact11, project: project, project_variable: project_var1, updated_at: Time.now
+
+          Contact.remove_duplicate_address_and_keep_last_update(project, project_var1)
+
+          expect(Contact.count).to eq(1)
+          expect(Contact.last.id).to eq(@contact11.id)  
+          expect(@contact11.reload.addresses.count).to eq(2)
+        end
+      end
+    end
+
+    context "keep the last modified" do
+      context "only one primary address" do
+        before(:each) do
+          @contact1 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "01000")]
+          @contact2 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "1000")]
+
+          PersistedVariable.make contact: @contact1, project: project, project_variable: project_var1, updated_at: Time.now
+          PersistedVariable.make contact: @contact2, project: project, project_variable: project_var1, updated_at: Time.now + 1.days
+        end
+
+        it { expect(Contact.count).to eq(2)}
+
+        it "should remove contact that is not last modified" do
+          Contact.remove_duplicate_address_and_keep_last_update(project, project_var1)
+
+          expect(Contact.count).to eq(1)
+          expect(Contact.last.id).to eq(@contact2.id)
+        end
+      end
+
+      context "has many addresses" do
+        before(:each) do
+          @contact1 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "1000"), ContactAddress.create(project: project, address: "2000")]
+          @contact2 = Contact.make project: project, addresses: [ContactAddress.create(project: project, address: "3000"), ContactAddress.create(project: project, address: "8551000")]
+
+          PersistedVariable.make contact: @contact1, project: project, project_variable: project_var1, updated_at: Time.now
+          PersistedVariable.make contact: @contact2, project: project, project_variable: project_var1, updated_at: Time.now + 1.days
+        end
+
+        it { expect(Contact.count).to eq(2)}
+
+        it "should remove address that has duplicated" do
+          Contact.remove_duplicate_address_and_keep_last_update(project, project_var1)
+
+          expect(Contact.count).to eq(2)
+
+          expect(Contact.first.id).to eq(@contact1.id)
+          expect(@contact1.addresses.count).to eq(1)
+          expect(Contact.last.id).to eq(@contact2.id)
+          expect(@contact2.addresses.count).to eq(2)
+        end
+      end
     end
   end
 
