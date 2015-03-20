4c85854694b3e2c08c8b14de2b503d25ca081877
Channa Ly <channa.info@gmail.com>
2015-03-20 11:51:11 +0700

    Update capistrano deploy.rb to solve symlink problem

--- config/deploy.rb
+++ config/deploy.rb
@@ -95,10 +95,10 @@ namespace :foreman do
   end
 end
 
-
+before 'deploy:finalize_update', "deploy:symlink_configs"
 before "deploy:start", "deploy:migrate"
 before "deploy:restart", "deploy:migrate"
-after "deploy:update_code", "deploy:symlink_configs"
+# after "deploy:update_code", "deploy:symlink_configs"
 after "deploy:update_code", "deploy:symlink_data"
 after "deploy:update_code", "deploy:prepare_broker"
 after "deploy:update_code", "deploy:compile_broker"
