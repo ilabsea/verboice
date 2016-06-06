ade8615ac670f817550f1fb7f6919361da13237d
juanboca <jblandini@manas.com.ar>
2016-06-06 15:39:56 +0700

    Updating schema.db to the current last migration, to fix the Travis build

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151030102826) do
+ActiveRecord::Schema.define(:version => 20160422032333) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
