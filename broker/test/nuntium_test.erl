f3d4e9efa4ef6c018381895d013acdb2086c278e
juanboca <jblandini@manas.com.ar>
2016-06-10 11:34:56 +0700

    Fixing nuntium_test on broker. Adding missing parameters to the nuntium:run function call

--- broker/test/nuntium_test.erl
+++ broker/test/nuntium_test.erl
@@ -9,10 +9,12 @@ should_work_with_text_localized_resource_test() ->
   meck:new(nuntium_api, [stub_all]),
   meck:new(resource),
   meck:expect(resource, prepare, [resource_guid, '_'], {text, en, "some text"}),
+  meck:new(nuntium_channel, [stub_all]),
+  meck:expect(nuntium_channel, find, [[{default, 1}, {account_id, 1}]], #nuntium_channel{channel_name = "Testing"}),
+  
+  {next, #session{}} = nuntium:run([{rcpt_type, caller}, {kind, "qst_server"}, {resource_guid, resource_guid}, {subject_guid, resource_guid}], Session),
 
-  {next, #session{}} = nuntium:run([{rcpt_type, caller}, {resource_guid, resource_guid}], Session),
-
-  ?assert(meck:called(nuntium_api, send_ao, [[{from, <<"sms://verboice">>}, {to, <<"sms://123">>}, {body, "some text"}, {account_id, 1}]])),
+  ?assert(meck:called(nuntium_api, send_ao, [[{from, <<"sms://verboice">>}, {to, <<"sms://123">>}, {subject, "some text"}, {body, "some text"}, {account_id, 1}, {suggested_channel, "Testing"}]])),
   meck:unload().
 
 should_send_to_a_value_recipient_test() ->
@@ -21,8 +23,10 @@ should_send_to_a_value_recipient_test() ->
   meck:new(nuntium_api, [stub_all]),
   meck:new(resource),
   meck:expect(resource, prepare, [resource_guid, '_'], {text, en, "some text"}),
+  meck:new(nuntium_channel, [stub_all]),
+  meck:expect(nuntium_channel, find, [[{default, 1}, {account_id, 1}]], #nuntium_channel{channel_name = "Testing"}),
 
-  {next, #session{}} = nuntium:run([{rcpt_type, expr}, {resource_guid, resource_guid}, {expr, "555"}], Session),
+  {next, #session{}} = nuntium:run([{rcpt_type, expr}, {kind, "qst_server"}, {resource_guid, resource_guid}, {expr, "555"}, {subject_guid, resource_guid}], Session),
 
-  ?assert(meck:called(nuntium_api, send_ao, [[{from, <<"sms://verboice">>}, {to, <<"sms://555">>}, {body, "some text"}, {account_id, 1}]])),
+  ?assert(meck:called(nuntium_api, send_ao, [[{from, <<"sms://verboice">>}, {to, <<"sms://555">>}, {subject, "some text"}, {body, "some text"}, {account_id, 1}, {suggested_channel, "Testing"}]])),
   meck:unload().
