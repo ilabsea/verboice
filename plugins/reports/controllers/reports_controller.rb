511a202a1e727ee3bb812af4e47bc30bb664d3d0
Sokmesa KHIEV <mesa@instedd.org>
2018-09-11 14:49:11 +0700

    handle basic authentication and white list

--- plugins/reports/controllers/reports_controller.rb
+++ plugins/reports/controllers/reports_controller.rb
@@ -1,6 +1,7 @@
 class ReportsController < ApplicationController
   before_filter :authenticate_account!, :except => [:call_finished]
-
+  before_filter :authenticate_basic!, :only => [:call_finished]
+  before_filter :authenticate_white_list!, :only => [:call_finished]
   def call_finished
     # params = {"CallSid"=>"391", "CallStatus"=>"completed", "From"=>"verboice_13", "CallDuration"=>"10", "action"=>"call_finished", "controller"=>"wit_ais"}
     finished_calls = ['completed', 'failed']
@@ -11,12 +12,30 @@ class ReportsController < ApplicationController
       message = Service::from_voice_to_speech(audio_file, params['CallSid'])
       # message = "There are two cases of headaches and eye problems and one case of wheezing in Hanoi"
       json_response = Service::from_speech_to_understanding message
-      parser = WitAiParser.new(params['CallSid'])
-      parser.parse(json_response)
-      report = parser.to_report_object()
-      report.save!
+      begin
+        parser = WitAiParser.new(params['CallSid'])
+        parser.parse(json_response)
+        report = parser.to_report_object()
+        report.save!
+      rescue
+        # report.create!(:)
+      end
       render :json => json_response
     end
+  end
+
+  def authenticate_basic! 
+    authenticate_or_request_with_http_basic do |user, password|
+      account = Account.find_by_email(user)
+      if account.valid_password?(password)
+        current_account = account
+        return
+      end
+    end
+  end
+
+  def authenticate_white_list!
+    head 403 if request.remote_ip != Reports::Settings.verboice_ip
   end
 
 end
--- plugins/reports/models/wit_ai_parser.rb
+++ plugins/reports/models/wit_ai_parser.rb
@@ -5,22 +5,26 @@ class WitAiParser
 	end
 
 	def parse(response)
+		case_key = Reports::Settings.case_key
+		number_of_case_key = Reports::Settings.number_of_case_key
+		symptoms_key = Reports::Settings.symptoms_key
+		location_key = Reports::Settings.location_key
 		index = 0
-		if response["entities"][Reports::Settings.case_key]
-			cases = response["entities"][Reports::Settings.case_key] 
+		if response["entities"][case_key]
+			cases = response["entities"][case_key] 
 			@properties = {}
 			cases.each do |obj|
 	    	@properties[index] = {}
-	    	@properties[index][Reports::Settings.number_of_case_key] = obj["entities"][Reports::Settings.number_of_case_key][0]["value"] if obj["entities"][Reports::Settings.number_of_case_key]
-	    	@properties[index][Reports::Settings.symptoms_key] = obj["entities"][Reports::Settings.symptoms_key].map {|s| s["value"]} if obj["entities"][Reports::Settings.symptoms_key]
+	    	@properties[index][number_of_case_key] = obj["entities"][number_of_case_key][0]["value"] if obj["entities"][number_of_case_key]
+	    	@properties[index][symptoms_key] = obj["entities"][symptoms_key].map {|s| s["value"]} if obj["entities"][symptoms_key]
 	      index = index + 1;
 	    end
 	  end
 	  @properties[index] = {}
-  	@properties[index][Reports::Settings.number_of_case_key] = response["entities"][Reports::Settings.number_of_case_key][0]["value"] if response["entities"][Reports::Settings.number_of_case_key]
-  	@properties[index][Reports::Settings.symptoms_key] = response["entities"][Reports::Settings.symptoms_key].map {|s| s["value"]} if response["entities"][Reports::Settings.symptoms_key]
-  	@properties.delete(index) if @properties[index][Reports::Settings.number_of_case_key] == nil and @properties[index][Reports::Settings.symptoms_key] == nil
-	  @location = response["entities"][Reports::Settings.location_key] ? response["entities"][Reports::Settings.location_key][0]["value"] : ""
+  	@properties[index][number_of_case_key] = response["entities"][number_of_case_key][0]["value"] if response["entities"][number_of_case_key]
+  	@properties[index][symptoms_key] = response["entities"][symptoms_key].map {|s| s["value"]} if response["entities"][symptoms_key]
+  	@properties.delete(index) if @properties[index][number_of_case_key] == nil and @properties[index][symptoms_key] == nil
+	  @location = response["entities"][location_key] ? response["entities"][location_key][0]["value"] : ""
     @message = response["_text"]
 	end
 
